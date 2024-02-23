3332e815bbba00f93860c3e33ebe121f4cb9089d
DJMogens <dani788c@gmail.com>
2024-02-23 09:10:26 +0100

    Update dependencies and add sidebar functionality

--- fdca-intranet/src/routes/+layout.svelte
+++ fdca-intranet/src/routes/+layout.svelte
@@ -1,6 +1,24 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import {
+		AppShell,
+		AppBar,
+		Avatar,
+		Modal,
+		type ModalSettings,
+		AppRail,
+		AppRailTile,
+		AppRailAnchor
+	} from '@skeletonlabs/skeleton';
+	import { faSearch, faBars, faHome } from '@fortawesome/free-solid-svg-icons'; // Import the faBars icon
+	import Fa from 'svelte-fa';
+	import img from '$lib/images/fdca_logo.svg';
+
+	import { initializeStores } from '@skeletonlabs/skeleton';
+	initializeStores();
+	import { getModalStore } from '@skeletonlabs/skeleton';
+
+	const modalStore = getModalStore();
 
 	// Highlight JS
 	import hljs from 'highlight.js/lib/core';
@@ -20,45 +38,85 @@
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
+
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	const searchModal: ModalSettings = {
+		type: 'prompt',
+		// Data
+		title: 'Enter Name',
+		body: 'Provide your first name in the field below.',
+		// Populates the input value and attributes
+		value: 'Skeleton',
+		valueAttr: { type: 'text', minlength: 3, maxlength: 10, required: true },
+		// Returns the updated response value
+		response: (r: string) => console.log('response:', r)
+	};
+
+	let sidebarActive = false;
+
+	function toggleSidebar() {
+		sidebarActive = !sidebarActive;
+	}
+
+	let currentTile = 0;
 </script>
 
+<!-- Modal -->
+<Modal />
+
 <!-- App Shell -->
-<AppShell>
+<AppShell slotPageHeader="h-0.5">
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<div class="flex items-center justify-center w-16">
+					<Fa
+						icon={faBars}
+						class="fa hover:text-blue-500"
+						style="font-size: 24px; padding-right: 30px; cursor: pointer;"
+						on:click={toggleSidebar}
+					/>
+				</div>
+				<a href="/">
+					<img src={img} alt="FDCA Logo" style="height: 40px;" />
+				</a>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
+				<button class="btn variant-filled">Kontakt</button>
+				<button class="btn variant-filled" on:click={() => modalStore.trigger(searchModal)}>
+					&nbsp&nbsp&nbspSearch&nbsp&nbsp&nbsp&nbsp&nbsp;
+					<Fa icon={faSearch} class="fa" />
+				</button>
+				<Avatar class="w-11" initials="JD" background="bg-primary-500"></Avatar>
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
-	<!-- Page Route Content -->
+	<svelte:fragment slot="sidebarLeft">
+		<div id="sidebar-left" class="{sidebarActive ? 'block' : 'hidden'} lg:block">
+			<AppRail width="w-16">
+				<!-- --- -->
+				<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
+					<svelte:fragment slot="lead">(icon)</svelte:fragment>
+					<span>Tile 1</span>
+				</AppRailTile>
+				<AppRailTile bind:group={currentTile} name="tile-2" value={1} title="tile-2">
+					<svelte:fragment slot="lead">(icon)</svelte:fragment>
+					<span>Tile 2</span>
+				</AppRailTile>
+				<AppRailTile bind:group={currentTile} name="tile-3" value={2} title="tile-3">
+					<svelte:fragment slot="lead">(icon)</svelte:fragment>
+					<span>Tile 3</span>
+				</AppRailTile>
+				<svelte:fragment slot="trail">
+					<AppRailAnchor href="/" class="flex flex-col items-center">
+						<Fa icon={faHome} class="fa" />
+						<span class="mt-1">Home</span>
+					</AppRailAnchor>
+				</svelte:fragment>
+			</AppRail>
+		</div>
+	</svelte:fragment>
 	<slot />
 </AppShell>
