a73e757a9c582aba26c4d450d285b1d99c4500fa
DJMogens <dani788c@gmail.com>
2024-03-12 14:11:53 +0100

    Disable modal click outside to clear in login modal

--- fdca-intranet/src/routes/Login.svelte
+++ fdca-intranet/src/routes/Login.svelte
@@ -1,30 +1,35 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import LoginModal from './LoginModal.svelte';
-	import { getAuth, onAuthStateChanged } from 'firebase/auth';
-	import { getModalStore, type ModalComponent, type ModalSettings } from '@skeletonlabs/skeleton';
-  
-	const modalStore = getModalStore();
-	const auth = getAuth();
-  
-	const modalComponent: ModalComponent = { ref: LoginModal };
-  
-	const modal: ModalSettings = {
-	  type: 'component',
-	  component: modalComponent,
-	};
-  
-	onMount(() => {
-	  // Listen for authentication state changes
-	  onAuthStateChanged(auth, (user) => {
-		if (user) {
-		  // User is signed in, clear the modal
-		  modalStore.clear();
-		} else {
-		  // No user is signed in, trigger the modal
-		  modalStore.trigger(modal);
-		}
-	  });
-	});
-  </script>
+    import { onMount } from 'svelte';
+    import LoginModal from './LoginModal.svelte';
+    import { getAuth, onAuthStateChanged } from 'firebase/auth';
+    import { getModalStore, type ModalComponent, type ModalSettings } from '@skeletonlabs/skeleton';
+
+    const modalStore = getModalStore();
+    const auth = getAuth();
+
+    let userLoggedIn = false;
+
+    const modalComponent: ModalComponent = { ref: LoginModal };
+
+    const modal: ModalSettings = {
+        type: 'component',
+        component: modalComponent,
+    };
+
+    onMount(() => {
+        // Listen for authentication state changes
+        onAuthStateChanged(auth, (user) => {
+            userLoggedIn = !!user;
+            if (userLoggedIn) {
+                // User is signed in, clear the modal
+                modalStore.clear();
+            } else {
+                // No user is signed in, trigger the modal
+                modalStore.trigger(modal);
+            }
+        });
+
+
+    });
+</script>
   
--- fdca-intranet/src/routes/LoginModal.svelte
+++ fdca-intranet/src/routes/LoginModal.svelte
@@ -8,12 +8,11 @@
     }
 </script>
 
-<style>
-    /* Optional: Add custom styles here if needed */
-</style>
 
-<button 
-    class="inline-flex items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline dark:bg-gray-800 dark:hover:bg-gray-700"
-    on:click={loginWithGoogle}>
-    <Fa icon={faGoogle} class="mr-2" /> Login with Google
-</button>
+<div class="h-screen w-screen flex justify-center items-center fixed bg-black bg-opacity-80">
+    <button 
+        class="inline-flex items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline dark:bg-gray-800 dark:hover:bg-gray-700"
+        on:click={loginWithGoogle}>
+        <Fa icon={faGoogle} class="mr-2" /> Login with Google
+    </button>
+</div>
