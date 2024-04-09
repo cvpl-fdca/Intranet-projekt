de4a5b16276199d0e62f8ecc0e0ddcde40129f8f
DJMogens <dani788c@gmail.com>
2024-04-09 17:05:09 +0200

    Add async keyword to getUid function and enhance forum post creation

--- fdca-intranet/src/routes/forum/[post]/+page.svelte
+++ fdca-intranet/src/routes/forum/[post]/+page.svelte
@@ -1,18 +1,33 @@
 <script lang="ts">
 	import MarkdownRenderer from '$lib/MarkdownRenderer.svelte';
 	import app from '$lib/firebase';
-	import { getFirestore, collection, doc, getDoc, onSnapshot } from 'firebase/firestore';
+	import {
+		getFirestore,
+		collection,
+		doc,
+		getDoc,
+		onSnapshot,
+		getDocs,
+		type DocumentData
+	} from 'firebase/firestore';
 	import type { PageData } from './$types';
 	import { writable } from 'svelte/store';
 	import { onMount } from 'svelte';
 	import { getToken, getUid } from '$lib/login';
 	import EditForumPost from '$lib/EditForumPost.svelte';
 	import { getModalStore, type ModalComponent, type ModalSettings } from '@skeletonlabs/skeleton';
+	import { userProfileStore } from '$lib/userProfileStore';
 
 	export let data: PageData;
-
+	let currentMessage = '';
 	console.log(data);
+	let userID: string | undefined;
+	userProfileStore.subscribe((value) => {
+		userID = value?.uid;
+	});
 
+	$: console.log('uid', userID);
+	
 	let db = getFirestore(app);
 	let title = writable('');
 	let markdownText = writable('');
@@ -20,6 +35,8 @@
 	let uid = writable('');
 	let authorName = writable('');
 	let time = writable('');
+	let comments = writable<DocumentData[]>([]);
+
 	getUid()
 		.then((uidValue) => {
 			uid.set(uidValue);
@@ -41,6 +58,23 @@
 				console.log(markdownText);
 			}
 		});
+
+		// Subscribe to comments subcollection
+		const commentsRef = collection(postRef, 'comments');
+
+		onSnapshot(commentsRef, (snapshot) => {
+			let commentsData = snapshot.docs.map((doc) => {
+				let data = doc.data();
+				if (typeof data.time === 'string') {
+					data.time = new Date(data.time); // Parse string to Date
+				}
+				return data;
+			});
+
+			commentsData.sort((a, b) => b.time - a.time); // Sort comments by time
+			comments.set(commentsData);
+		});
+
 		return unsubscribe;
 	};
 
@@ -90,6 +124,23 @@
 			console.error('Error:', error.message);
 		}
 	}
+	async function addComment() {
+		try {
+			const token = await getToken();
+			const submissionFormData = new FormData();
+			submissionFormData.append('text', currentMessage);
+			// Append the postID to the URL as a parameter
+			const response = await fetch(`/api/forum/addComment/${data.post}`, {
+				method: 'POST',
+				headers: {
+					'X-firebase-token': token
+				},
+				body: submissionFormData
+			});
+		} catch (error) {
+			console.error('Error:', error.message);
+		}
+	}
 </script>
 
 {#if $uid === $authorUID}
@@ -102,3 +153,35 @@
 <p>{new Date($time).toLocaleString()}</p>
 
 <MarkdownRenderer {markdownText} />
+
+<div class="grid gap-1 h-auto w-auto">
+    <div class="bg-surface-500/30 p-4 overflow-y-auto">
+        {#each $comments as comment}
+            <div class="grid gap-2">
+                <div class={`card p-4  rounded-tl-none space-y-2 my-2 ${userID === comment.authorUID ? 'variant-ghost' : 'variant-soft'}`}>
+                    <!-- Added 'my-2' class for margin -->
+                    <header class="flex justify-between items-center">
+                        <p class="font-bold">{comment.authorName}</p>
+
+                        <small class="opacity-50">{new Date(comment.time).toLocaleString()}</small>
+                    </header>
+                    <p>{comment.text}</p>
+                </div>
+            </div>
+        {/each}
+    </div>
+	<div class="bg-surface-500/30 p-4">
+		<div class="input-group input-group-divider grid-cols-[auto_1fr_auto] rounded-container-token">
+			<button class="input-group-shim">+</button>
+			<textarea
+				bind:value={currentMessage}
+				class="bg-transparent border-0 ring-0"
+				name="prompt"
+				id="prompt"
+				placeholder="Write a message..."
+				rows="1"
+			/>
+			<button class="variant-filled-primary" on:click={addComment}>Send</button>
+		</div>
+	</div>
+</div>
