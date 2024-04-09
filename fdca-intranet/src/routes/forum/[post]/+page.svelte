82749232e952bfe66a3286f9e5a23cbf9927e24f
Philipsd01 <philip.dalager@gmail.com>
2024-04-09 15:42:40 +0200

    Forum post page design updated and post view page table of contents added

--- fdca-intranet/src/routes/forum/[post]/+page.svelte
+++ fdca-intranet/src/routes/forum/[post]/+page.svelte
@@ -6,8 +6,12 @@
 	import { writable } from 'svelte/store';
 	import { onMount } from 'svelte';
 	import { getToken, getUid } from '$lib/login';
+	import { tocCrawler, TableOfContents } from '@skeletonlabs/skeleton';
+	import { AppShell } from '@skeletonlabs/skeleton';
 
 	export let data: PageData;
+
+
 
 	console.log(data);
 
@@ -24,7 +28,6 @@
 		.catch((error) => {
 			console.error('Error getting UID:', error);
 		});
-
 
 	const fetchData = async () => {
 		const postRef = doc(db, 'OpenForum', data.post);
@@ -44,29 +47,53 @@
 		fetchData();
 	});
 
-	
 	async function deletePost() {
-    try {
-      const token = await getToken();
-      // Append the postID to the URL as a parameter
-      const response = await fetch(`/api/forum/deleteForumPost/${data.post}`, {
-        method: 'DELETE',
-        headers: {
-          'X-firebase-token': token
-        }
-      });
-    } catch (error) {
-      console.error('Error:', error.message);
-    }
-  }
-
+		try {
+			const token = await getToken();
+			// Append the postID to the URL as a parameter
+			const response = await fetch(`/api/forum/deleteForumPost/${data.post}`, {
+				method: 'DELETE',
+				headers: {
+					'X-firebase-token': token
+				}
+			});
+		} catch (error) {
+			console.error('Error:', error.message);
+		}
+	}
 </script>
 
+
+<AppShell>
+	<svelte:fragment slot="sidebarLeft">
+    <!-- Table of Contents -->
+    <div use:tocCrawler={{ mode: 'generate' }} class="w-1/8' mr-4 flex-none overflow-x-hidden overflow-y-auto">
+      <h2>Heading 2</h2>
+      <p>...</p>
+      <h3>Heading 3</h3>
+      <p>...</p>
+      <!-- Table of contents will be generated here -->
+    </div>
+	<TableOfContents></TableOfContents>
+  </svelte:fragment>
+
+  <slot>
+	<div class="container mx-auto my-8">
+    <div class="flex justify-center">
+      <!-- Post Content -->
+      <div class="flex flex-col items-center w-3/4">
+        <h1 class="max-w-md px-4 mb-4">{$title}</h1>
+        <div class="markdown-container w-[850px] px-4 py-2">
+          <MarkdownRenderer {markdownText} />
+        </div>
+      </div>
+    </div>
+  </div>
+  </slot>
+  
+</AppShell>
+
 {#if $uid === $authorUID}
-  <button on:click={deletePost} type="button" class="btn variant-filled">Delete</button>
-  <button type="button" class="btn variant-filled">Edit</button>
+	<button on:click={deletePost} type="button" class="btn variant-filled">Delete</button>
+	<button type="button" class="btn variant-filled">Edit</button>
 {/if}
-
-<h1>{$title}</h1>
-
-<MarkdownRenderer {markdownText} />
