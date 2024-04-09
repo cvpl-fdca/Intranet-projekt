cf5005000fcb362d936b482282c54c30b6c34d3a
lyngdorf <sofie_lyngdorf@hotmail.com>
2024-04-09 13:21:39 +0200

    Added drop down menu to /kontakt which redirects to /kontakt/medlemmer or /kontakt/forslag

--- fdca-intranet/src/routes/+layout.svelte
+++ fdca-intranet/src/routes/+layout.svelte
@@ -11,13 +11,15 @@
 		AppRailTile,
 		AppRailAnchor,
 		type DrawerSettings,
-		Drawer
+		Drawer,
+		type PopupSettings,
+		popup
 	} from '@skeletonlabs/skeleton';
 	import { faSearch, faBars, faHome } from '@fortawesome/free-solid-svg-icons'; // Import the faBars icon
 	import Fa from 'svelte-fa';
 	import img from '$lib/images/fdca_logo.svg';
 
-	import { initializeStores } from '@skeletonlabs/skeleton';
+	import { initializeStores, ListBox, ListBoxItem } from '@skeletonlabs/skeleton';
 	initializeStores();
 	import { getModalStore } from '@skeletonlabs/skeleton';
 
@@ -89,6 +91,22 @@
 		console.log('sidebarActive:', sidebarActive);
 	}
 
+	let comboboxKontakt: string = "Kontakt";
+	function handleListBoxClick(value: string) {
+		if (value === 'medlemmer') {
+			window.location.href = '/kontakt/medlemmer';
+		} else if (value === 'forslag') {
+			window.location.href = '/kontakt/forslag';
+		}
+	};
+
+	const popupComboKontakt: PopupSettings = {
+		event: 'click',
+		target: 'popupComboKontakt',
+		placement: 'bottom',
+		closeQuery: '.listbox-item'
+	};
+
 	let currentTile = 0;
 </script>
 
@@ -100,6 +118,14 @@
 <Drawer>
 	<Profile />
 </Drawer>
+
+<div class="card w-48 shadow-xl py-2" data-popup="popupComboKontakt">
+	<ListBox rounded="rounded-none">
+		<ListBoxItem bind:group={comboboxKontakt} name="medium" value="medlemmer" on:click={() => handleListBoxClick('medlemmer')}>Medlemmer</ListBoxItem>
+		<ListBoxItem bind:group={comboboxKontakt} name="medium" value="forslag" on:click={() => handleListBoxClick('forslag')}>Forslag</ListBoxItem>
+	</ListBox>
+	<div class="arrow bg-surface-100-800-token" />
+</div>
 
 <!-- App Shell -->
 <AppShell slotPageHeader="h-0.5">
@@ -117,7 +143,10 @@
 			<svelte:fragment slot="trail">
 				<a href="/forum" class="btn variant-filled"> Forum </a>
 				<a href="/artikler" class="btn variant-filled"> Artikler </a>
-				<a href="/kontakt" class="btn variant-filled"> Kontakt </a>
+				<button class="btn variant-filled justify-between" use:popup={popupComboKontakt}>
+					<span href="/kontakt/medlemmer" class="capitalize">{comboboxKontakt ?? 'Trigger'}</span>
+					<span>↓</span>
+				</button>
 				{#if $userProfileStore?.roles.isAdmin}
 					<a href="/admin" class="btn variant-filled"> Admin </a>
 				{/if}
