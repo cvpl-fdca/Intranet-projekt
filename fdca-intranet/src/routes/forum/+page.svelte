64ec80d1549db2de8f182999fa7b3a9a74c5e0b0
Soya <Axewu_Reitzel@hotmail.com>
2024-04-11 15:24:15 +0200

    Add dropdown menu for sorting posts in forum page (doesn't work) + forum page layout changes

--- fdca-intranet/src/routes/forum/+page.svelte
+++ fdca-intranet/src/routes/forum/+page.svelte
@@ -9,6 +9,10 @@
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
 	import { ImagePlaceholder } from 'flowbite-svelte';
+
+	import { Dropdown, DropdownItem, DropdownDivider, DropdownHeader } from 'flowbite-svelte';
+	import { AngleDownOutline} from 'flowbite-svelte-icons';
+
 	let db = getFirestore(app);
 
 	// Subscribe to updates in the 'OpenForum' collection
@@ -45,11 +49,36 @@
 	}
 
 	modalStore.close();
+
+	let selected = 'Sort';
+  	const selectItem = (item) => selected = item;
+
 </script>
 
-<h1>Forum!!</h1>
 
-<button type="button" class="btn variant-filled" on:click={openModal}>Add a post</button>
+<style>
+	.center-content {
+	  display: flex;
+	  flex-direction: column;
+	  align-items: center;
+	  justify-content: center;
+	  gap: 10px;
+	  height: 200px;
+	}
+  </style>
+  
+
+  
+<div class="center-content">
+	<h1>Forum</h1>
+	<button type="button" class="btn variant-filled" on:click={openModal}>Add a post</button>
+	<button type="button" class="btn" >{selected}<AngleDownOutline/></button>
+	<Dropdown>
+		<DropdownItem on:click={() => selectItem('Newest')}>Newest</DropdownItem>
+  		<DropdownItem on:click={() => selectItem('Oldest')}>Oldest</DropdownItem>
+  		<DropdownItem on:click={() => selectItem('Best')}>Best</DropdownItem>
+	</Dropdown>
+</div>
 
 <div class="grid grid-cols-3 gap-4">
 	{#each $posts as post}
