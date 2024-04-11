3aef9b1c018768067eb3d169d28c58c46815b1db
DJMogens <dani788c@gmail.com>
2024-04-11 14:01:01 +0200

    Make the data in /kontakt dynamic and fetch from firebase /groups

--- fdca-intranet/src/routes/kontakt/+page.svelte
+++ fdca-intranet/src/routes/kontakt/+page.svelte
@@ -1,67 +1,69 @@
 <script lang="ts">
+	import { writable } from 'svelte/store';
 	import { getDrawerStore, type DrawerSettings } from '@skeletonlabs/skeleton';
 	import app from '$lib/firebase';
 	import { getDocs, collection, onSnapshot, getFirestore, getDoc, doc } from 'firebase/firestore';
 	import { onDestroy } from 'svelte';
-
-	interface BoardMember {
-		name: string;
-		phone: string;
-		address: string;
-		imageUrl: string;
-		additionalText: string;
-	}
-	interface CommitteeMember {
-		name: string;
-		phone: string;
-		address: string;
-		imageUrl: string;
-		additionalText: string;
-	}
-
-	
-
+	import { User } from '$lib/user';
 
 	const db = getFirestore(app);
-	// Get a reference to the 'groups' collection
 	const groupsCollection = collection(db, 'groups');
+	const committees = writable([]); // Create a writable store for the committees
+	const bestyrelse = writable({ name: '', members: [] }); // Create a separate store for the "bestyrelse" committee
 
-	// Get a reference to the 'bestyrelse' document
-	const bestyrelseDocRef = doc(groupsCollection, 'bestyrelse');
-
-	// Fetch the 'bestyrelse' document
-	const bestyrelseDocSnap = getDoc(bestyrelseDocRef);
-
-	// Get the 'members' map
-	const members = bestyrelseDocSnap.data().members;
-
-	// Initialize an empty array for the board members
-	let boardMembers = [] as BoardMember[];
-
-	// For each member in the 'members' map
-	for (const member in members) {
-		// Get the user document reference
-		const userDocRef = members[member];
-
-		// Fetch the user document
-		const userDocSnap = await getDoc(userDocRef);
-
-		// Get the user data
-		const userData = userDocSnap.data();
-
-		// Add the user data to the board members array
-		boardMembers.push({
-			name: userData.name,
-			phone: userData.phone,
-			address: userData.address,
-			imageUrl: userData.imageUrl,
-			additionalText: userData.additionalText
-		});
+	$: console.log($committees);
+	$: console.log($bestyrelse);
+	// Define the Committee interface
+	interface Committee {
+		name: string;
+		members: User[];
 	}
 
+	// Subscribe to the 'groups' collection
+	const unsubscribe = onSnapshot(groupsCollection, async (snapshot) => {
+		let newCommittees: Committee[] = [];
+
+		// For each document in the 'groups' collection
+		for (const doc of snapshot.docs) {
+			let committee: Committee = {
+				name: doc.id,
+				members: []
+			};
+
+			// Get the 'members' map
+			const members = doc.data().members;
+
+			// For each member in the 'members' map
+			for (const member in members) {
+				// Get the user document reference
+				const userDocRef = members[member];
+
+				// Fetch the user document
+				const userDocSnap = await getDoc(userDocRef);
+
+				// Get the user data
+				const userData = userDocSnap.data() as User;
+
+				committee.members.push(userData);
+			}
+
+			// If the committee is "bestyrelse", update the "bestyrelse" store
+			if (committee.name === 'bestyrelse') {
+				bestyrelse.set(committee);
+			} else {
+				newCommittees.push(committee);
+			}
+		}
+
+		// Update the committees store
+		committees.set(newCommittees);
+	});
+
 	onDestroy(() => {
+		// Unsubscribe from the 'groups' collection when the component is destroyed
 		unsubscribe();
 	});
+
 	const drawerStore = getDrawerStore();
 
 	const drawerKontakt: DrawerSettings = {
@@ -78,102 +80,66 @@
 	function openKontaktDrawer() {
 		drawerStore.open(drawerKontakt);
 	}
-
-
-
-	// Example array of committee members
-	let committeeMembers: CommitteeMember[] = [
-		{
-			name: 'Who',
-			phone: '123-456-7890',
-			address: '123 Main St',
-			additionalText: 'Additional info',
-			imageUrl: '/path/to/existing/member/image.jpg'
-		},
-		{
-			name: 'Are',
-			phone: '123-456-7890',
-			address: '123 Main St',
-			additionalText: 'Additional info',
-			imageUrl: '/path/to/existing/member/image.jpg'
-		},
-		{
-			name: 'You',
-			phone: '123-456-7890',
-			address: '123 Main St',
-			additionalText: 'Additional info',
-			imageUrl: '/path/to/existing/member/image.jpg'
-		}
-	];
 </script>
 
-<div class="container mx-auto p-4">
-	<!-- Bestyrelse Section -->
-	<div class="card bg-blue-800 shadow-md rounded-lg p-4 mb-4">
-		<h3 class="text-3xl font-semibold text-white mb-4 text-center">Bestyrelse</h3>
-		<div class="text-center mb-4">
-			<button class="bg-white text-black py-2 px-8 rounded-full" on:click={openKontaktDrawer}
-				>Skriv til Bestyrelse</button
+<div class="container mx-auto p-4"></div>
+<!-- Bestyrelse Section -->
+<div class="card bg-blue-800 shadow-md rounded-lg p-4 mb-4">
+	<h3 class="text-3xl font-semibold text-white mb-4 text-center">Bestyrelsen</h3>
+	<div class="text-center mb-4">
+		<button class="bg-white text-black py-2 px-8 rounded-full" on:click={openKontaktDrawer}
+			>Skriv til Bestyrelse</button
+		>
+	</div>
+	<div
+		class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 justify-items-center justify-content-center"
+	>
+		{#each $bestyrelse.members as member, i (i)}
+			<div class="card p-4 bg-white rounded shadow-lg animate-fade-in">
+				<div class="flex flex-col items-center">
+					<img
+						class="w-24 h-24 rounded-full mb-3"
+						src={member.details.imageUrl}
+						alt={`Picture of ${member.details.fullName}`}
+					/>
+					<div class="text-center">
+						<h4 class="font-semibold">{member.details.fullName}</h4>
+						<p>{member.details.phone.private}</p>
+						<p class="text-sm">{member.details.email.fdca}</p>
+					</div>
+				</div>
+			</div>
+		{/each}
+	</div>
+</div>
+
+{#each $committees as committee}
+	<div class="container mx-auto p-4">
+		<div class="card bg-blue-800 shadow-md rounded-lg p-4 mb-4">
+			<h3 class="text-3xl font-semibold text-white mb-4 text-center">{committee.name}</h3>
+			<div
+				class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 justify-items-center justify-content-center"
 			>
-		</div>
-		<div class="grid grid-flow-row-dense grid-cols-auto-fit gap-4">
-			{#each boardMembers as member, i (i)}
-				<div class="card p-4 bg-white rounded shadow-lg">
-					<div class="flex flex-col items-center">
-						<img
-							class="w-24 h-24 rounded-full mb-3"
-							src={member.imageUrl}
-							alt={`Picture of ${member.name}`}
-						/>
-						<div class="text-center">
-							<h4 class="font-semibold">{member.name}</h4>
-							<p>{member.phone}</p>
-							<p class="text-sm">{member.address}</p>
-							<p class="text-sm">{member.additionalText}</p>
+				{#each committee.members as member, i (i)}
+					<div
+						class="card p-4 bg-white rounded shadow-lg animate-fade-in"
+						style="animation-delay: {i * 100}ms;"
+					>
+						<div class="flex flex-col items-center">
+							<img
+								class="w-24 h-24 rounded-full mb-3"
+								src={member.details.imageUrl}
+								alt={`Picture of ${member.details.fullName}`}
+							/>
+							<div class="text-center">
+								<h4 class="font-semibold">{member.details.fullName}</h4>
+								<p>{member.details.phone.private}</p>
+								<p class="text-sm">{member.details.email.fdca}</p>
+							</div>
 						</div>
 					</div>
-				</div>
-			{/each}
+				{/each}
+			</div>
 		</div>
 	</div>
-</div>
-
-<!-- Udvalg Section -->
-<div class="container mx-auto p-4">
-	<div class="card bg-blue-800 shadow-md rounded-lg p-4 mb-4">
-		<h3 class="text-3xl font-semibold text-white mb-4 text-center">Udvalg</h3>
-		<div class="grid grid-flow-row-dense grid-cols-auto-fit gap-4">
-			{#each committeeMembers as member, i (i)}
-				<div class="card p-4 bg-white rounded shadow-lg">
-					<div class="flex flex-col items-center">
-						<img
-							class="w-24 h-24 rounded-full mb-3"
-							src={member.imageUrl}
-							alt={`Picture of ${member.name}`}
-						/>
-						<div class="text-center">
-							<h4 class="font-semibold">{member.name}</h4>
-							<p>{member.phone}</p>
-							<p class="text-sm">{member.address}</p>
-							<p class="text-sm">{member.additionalText}</p>
-						</div>
-					</div>
-				</div>
-			{/each}
-		</div>
-	</div>
-</div>
-
-<style>
-	.grid-cols-auto-fit {
-		display: grid;
-		grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
-		grid-gap: 1rem;
-	}
-
-	.card {
-		transition: background-color 0.3s ease;
-		/* If the cards need a fixed width, you can specify it here, and they will maintain the width regardless of screen size */
-		/* width: 240px; */
-	}
-</style>
+{/each}
