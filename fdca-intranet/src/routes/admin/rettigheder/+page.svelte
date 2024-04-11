70adeb8429c42fec6cf734ec7b43fe616ec14743
DJMogens <dani788c@gmail.com>
2024-04-11 12:17:35 +0200

    Refactor table source and fetch users asynchronously

--- fdca-intranet/src/routes/admin/rettigheder/+page.svelte
+++ fdca-intranet/src/routes/admin/rettigheder/+page.svelte
@@ -4,13 +4,13 @@
 	import type { User } from '$lib/user';
 	import { userProfileStore } from '$lib/userProfileStore';
 	import { Table } from '@skeletonlabs/skeleton';
-	import type { TableSource} from '@skeletonlabs/skeleton';
+	import type { TableSource } from '@skeletonlabs/skeleton';
 	import { tableMapperValues } from '@skeletonlabs/skeleton';
 
 	let members: User[] = [];
 	$: {
 		members = $memberStore;
-	};
+	}
 
 	type Element = {
 		position: number;
@@ -21,34 +21,40 @@
 		strøko: boolean;
 		socsam: boolean;
 	};
-	
+
 	function setTableSource(): TableSource {
 		return {
-			head: ['Name', 'User', 'Admin', 'Karriere/Kompetence', 'Strategi/Økonomi', 'Socialt sammenhold'],
-			body: tableMapperValues(sourceData, ['name', 'user', 'isAdmin', 'karkom', 'socsam']),
-		}
-	};
+			head: [
+				'Name',
+				'User',
+				'Admin',
+				'Karriere/Kompetence',
+				'Strategi/Økonomi',
+				'Socialt sammenhold'
+			],
+			body: tableMapperValues(sourceData, ['name', 'user', 'isAdmin', 'karkom', 'socsam'])
+		};
+	}
 
 	function getSourceData(members: User[]): Element[] {
 		let sourceData: Element[] = [];
 		let i = 1;
 		let new_el: Element;
-		members.forEach(member => {
-			
+		members.forEach((member) => {
 			new_el = {
 				position: i,
 				name: member.details.fullName,
-				user: "FILLOUT",
+				user: 'FILLOUT',
 				isAdmin: member.roles.isAdmin,
 				karkom: member.roles.projects.karkom,
 				strøko: member.roles.projects.strøko,
-				socsam: member.roles.projects.socsam,
-			}
+				socsam: member.roles.projects.socsam
+			};
 			sourceData.push(new_el);
-			i+=1;
+			i += 1;
 		});
 		return sourceData;
-	};
+	}
 
 	async function getUsers() {
 		let userDoc;
@@ -56,31 +62,31 @@
 			userDoc = value as User;
 		});
 		try {
-		const token = await getToken();
+			const token = await getToken();
 
-		const response = await fetch('/api/admin/getUserIdentifier', {
-			method: 'GET',
-			headers: {
-				'X-firebase-token': token
-			}
-		});
-		console.log("Hello2");
-		console.log(response.body);
-		return response;
+			const response = await fetch('/api/admin/getUserIdentifier', {
+				method: 'GET',
+				headers: {
+					'X-firebase-token': token
+				}
+			});
+			console.log('Hello2');
+			console.log(response.then);
+			return response;
 		} catch (error) {
 			console.error('Error:', error.message);
 		}
-		
-	};
-	let users = getUsers();
-	
+	}
+
+	let users = getUsers().then((response) => {
+		console.log(response);
+	});
+
 	console.log(users);
 	let sourceData: Element[] = [];
 	let tableSimple: TableSource;
 	$: sourceData = getSourceData(members);
 	$: tableSimple = sourceData ? setTableSource() : undefined;
-
-
 </script>
 
 <Table source={tableSimple} />
