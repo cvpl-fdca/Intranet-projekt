81195536b869764acd99716afaaa68972f2d5b43
DJMogens <dani788c@gmail.com>
2024-04-11 11:57:41 +0200

    Update forum post page layout and functionality

--- fdca-intranet/src/routes/forum/[post]/+page.svelte
+++ fdca-intranet/src/routes/forum/[post]/+page.svelte
@@ -17,11 +17,9 @@
 	import EditForumPost from '$lib/EditForumPost.svelte';
 	import { getModalStore, type ModalComponent, type ModalSettings } from '@skeletonlabs/skeleton';
 	import { userProfileStore } from '$lib/userProfileStore';
-	import { tocCrawler, TableOfContents } from '@skeletonlabs/skeleton';
-	import { AppShell } from '@skeletonlabs/skeleton';
 
 	export let data: PageData;
-
+	let currentMessage = '';
 	console.log(data);
 	let userID: string | undefined;
 	userProfileStore.subscribe((value) => {
@@ -84,59 +82,106 @@
 		fetchData();
 	});
 
-	
-	async function deletePost() {
-    try {
-      const token = await getToken();
-      // Append the postID to the URL as a parameter
-      const response = await fetch(`/api/forum/deleteForumPost/${data.post}`, {
-        method: 'DELETE',
-        headers: {
-          'X-firebase-token': token
-        }
-      });
-    } catch (error) {
-      console.error('Error:', error.message);
-    }
-  }
+	const modalStore = getModalStore();
+	const editForumPost: ModalComponent = { ref: EditForumPost };
 
+	// Reactive statements
+	$: console.log($markdownText);
+	$: console.log($title);
+
+	let modal: ModalSettings;
+
+	// Reactive statement to update the modal object
+	$: {
+		modal = {
+			type: 'component',
+			component: editForumPost,
+			meta: {
+				postId: data.post,
+				title: $title,
+				markdownText: markdownText
+			}
+		};
+	}
+
+	async function openModal() {
+		modalStore.trigger(modal);
+	}
+
+	modalStore.close();
+
+	async function deletePost() {
+		try {
+			const token = await getToken();
+			// Append the postID to the URL as a parameter
+			const response = await fetch(`/api/forum/deleteForumPost/${data.post}`, {
+				method: 'DELETE',
+				headers: {
+					'X-firebase-token': token
+				}
+			});
+		} catch (error) {
+			console.error('Error:', error.message);
+		}
+	}
+	async function addComment() {
+		try {
+			const token = await getToken();
+			const submissionFormData = new FormData();
+			submissionFormData.append('text', currentMessage);
+			// Append the postID to the URL as a parameter
+			const response = await fetch(`/api/forum/addComment/${data.post}`, {
+				method: 'POST',
+				headers: {
+					'X-firebase-token': token
+				},
+				body: submissionFormData
+			});
+		} catch (error) {
+			console.error('Error:', error.message);
+		}
+	}
 </script>
 
-
-<AppShell>
-	<svelte:fragment slot="sidebarLeft">
-    <!-- Table of Contents -->
-    <div use:tocCrawler={{ mode: 'generate' }} class="w-1/8' mr-4 flex-none overflow-x-hidden overflow-y-auto">
-      <h2>Heading 2</h2>
-      <p>...</p>
-      <h3>Heading 3</h3>
-      <p>...</p>
-      <!-- Table of contents will be generated here -->
-    </div>
-	<TableOfContents></TableOfContents>
-  </svelte:fragment>
-
-  <slot>
-	<div class="container mx-auto my-8">
-    <div class="flex justify-center">
-      <!-- Post Content -->
-      <div class="flex flex-col items-center w-3/4">
-        <h1 class="max-w-md px-4 mb-4">{$title}</h1>
-        <div class="markdown-container w-[850px] px-4 py-2">
-          <MarkdownRenderer {markdownText} />
-        </div>
-      </div>
-    </div>
-  </div>
-  </slot>
-  
-</AppShell>
-
 {#if $uid === $authorUID}
-  <button on:click={deletePost} type="button" class="btn variant-filled">Delete</button>
-  <button type="button" class="btn variant-filled">Edit</button>
+	<button on:click={deletePost} type="button" class="btn variant-filled">Delete</button>
+	<button type="button" class="btn variant-filled" on:click={openModal}>Edit</button>
 {/if}
 
 <h1>{$title}</h1>
+<p>{$authorName}</p>
+<p>{new Date($time).toLocaleString()}</p>
 
 <MarkdownRenderer {markdownText} />
+
+<div class="grid gap-1 h-auto w-auto">
+    <div class="bg-surface-500/30 p-4 overflow-y-auto">
+        {#each $comments as comment}
+            <div class="grid gap-2">
+                <div class={`card p-4  rounded-tl-none space-y-2 my-2 ${userID === comment.authorUID ? 'variant-ghost' : 'variant-soft'}`}>
+                    <!-- Added 'my-2' class for margin -->
+                    <header class="flex justify-between items-center">
+                        <p class="font-bold">{comment.authorName}</p>
+
+                        <small class="opacity-50">{new Date(comment.time).toLocaleString()}</small>
+                    </header>
+                    <p>{comment.text}</p>
+                </div>
+            </div>
+        {/each}
+    </div>
+	<div class="bg-surface-500/30 p-4">
+		<div class="input-group input-group-divider grid-cols-[auto_1fr_auto] rounded-container-token">
+			<button class="input-group-shim">+</button>
+			<textarea
+				bind:value={currentMessage}
+				class="bg-transparent border-0 ring-0"
+				name="prompt"
+				id="prompt"
+				placeholder="Write a message..."
+				rows="1"
+			/>
+			<button class="variant-filled-primary" on:click={addComment}>Send</button>
+		</div>
+	</div>
+</div>
