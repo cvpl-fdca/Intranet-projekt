5ef6ff8fba97a5b569f8dd2c6404957cd07676d0
lyngdorf <sofie_lyngdorf@hotmail.com>
2024-04-12 14:22:25 +0200

    Article posting API works. We need to decide how to display

---
+++ fdca-intranet/src/lib/AddArticle.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import type { SvelteComponent } from 'svelte';
+	import app from '$lib/firebase';
+	import { getAuth, onAuthStateChanged } from 'firebase/auth';
+	
+	//import firestore
+	import { getFirestore, collection, addDoc } from 'firebase/firestore';
+	import { getModalStore } from '@skeletonlabs/skeleton';
+	import MarkdownEditor from '$lib/MarkdownEditor.svelte';
+	import { getToken } from '$lib/login';
+	import { userProfileStore } from './userProfileStore';
+	import type { User } from './user';
+	import { writable } from 'svelte/store';
+
+	let userDoc;
+	userProfileStore.subscribe((value) => {
+		userDoc = value as User;
+		console.log('userdoc' + userDoc);
+	});
+
+	const token = getToken();
+	let title: string;
+	let text = writable('');
+
+	export let parent: SvelteComponent;
+
+	const modalStore = getModalStore();
+
+	async function addArticle() {
+		try {
+			const token = await getToken();
+			const submissionFormData = new FormData();
+			submissionFormData.append('text', $text);
+			submissionFormData.append('title', title);
+			const response = await fetch('/api/artikler/createArticle', {
+				method: 'POST',
+				body: submissionFormData,
+				headers: {
+					'X-firebase-token': token
+				}
+			})
+		} catch (error) {
+			console.error('Error:', error.message);
+		}
+	}
+
+</script>
+
+{#if $modalStore[0]}
+  <div class="flex flex-col items-center justify-center">
+    <input
+      type="text"
+      bind:value={title}
+      class="w-[600px] p-2 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 mb-4"
+      placeholder="Title"
+    />
+
+    <div class="w-[600px] mb-4">
+      <MarkdownEditor {text} />
+    </div>
+
+    <button
+      on:click={addArticle}
+      type="submit"
+      class="w-[120px] inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
+    >
+      Publish post
+    </button>
+  </div>
+{/if}
+
---
+++ fdca-intranet/src/routes/api/artikler/createArticle/+server.ts
@@ -0,0 +1,86 @@
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+import { type DecodedIdToken } from 'firebase-admin/auth';
+import { admin } from '$lib/firebaseAdmin.server.js';
+import { json } from '@sveltejs/kit';
+import type { User } from '$lib/user';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+const currentTimeInCopenhagen = dayjs().tz('Europe/Copenhagen').format();
+console.log(currentTimeInCopenhagen);
+
+const db = admin.firestore();
+
+export async function POST(event) {
+    // Retrieve the Firebase token from the request headers
+    const firebaseToken = event.request.headers.get('X-firebase-token');
+    if (!firebaseToken) {
+        return new Response(JSON.stringify({ error: 'Firebase token not provided' }), {
+            status: 401,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    }
+    let username: string;
+    let token: DecodedIdToken;
+
+    try {
+        // Verify the Firebase token and decode it to get the UID
+        console.log('Verifying Firebase token:', firebaseToken);
+        const decodedToken = await admin.auth().verifyIdToken(firebaseToken);
+        token = decodedToken;
+        console.log('Successfully authenticated Firebase token from user:', token.email);
+    } catch (error) {
+        console.error('Error verifying Firebase token:', error);
+        return new Response(JSON.stringify({ error: 'Failed to authenticate Firebase token' }), {
+            status: 403,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    }
+
+    const data = await event.request.formData();
+    console.log('Form data', data);
+
+    //TODO: Validate form data
+    if (true) {
+        let userDoc = (await db.collection('users').doc(token.uid).get()).data() as User;
+        username = userDoc.details?.fullName;
+
+        let article = {
+            authorUID: token.uid,
+            authorName: username,
+            text: data.get('text') as string,
+            time: currentTimeInCopenhagen,
+            title: data.get('title') as string,
+        };
+
+        try {
+            const articleRef = await admin.firestore().collection('articles').doc('karkom').collection('posts').add(article);
+
+        } catch (error) {
+            console.error('Failed to create article:', error);
+            return new Response(JSON.stringify({error: 'Failed to create article'}), {
+                status: 500,
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+        }
+    } else {
+        return new Response(JSON.stringify("Error"), {
+            status: 400,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    }
+
+
+    return json({success: true});
+}
