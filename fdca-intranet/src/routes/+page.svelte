6eea2d5b8906d3572529bab8fe555300f242d942
lyngdorf <sofie_lyngdorf@hotmail.com>
2024-04-29 13:22:42 +0200

    Calendar works. Not very pretty.

--- fdca-intranet/src/routes/+page.svelte
+++ fdca-intranet/src/routes/+page.svelte
@@ -1,5 +1,62 @@
 <script lang="ts">
+    import { writable } from 'svelte/store';
+	import { getModalStore, type ModalComponent, type ModalSettings } from '@skeletonlabs/skeleton';
+    import AddEvent from '$lib/AddEvent.svelte';
+    import { onMount } from 'svelte';
+	import { getToken } from '$lib/login';
+    import MarkdownEditor from '$lib/MarkdownEditor.svelte';
+	import MarkdownRenderer from '$lib/MarkdownRenderer.svelte';
+	import app from '$lib/firebase';
+	import { getFirestore, collection, query, getDocs, onSnapshot } from 'firebase/firestore';
+	import { goto } from '$app/navigation';
+	import { ImagePlaceholder } from 'flowbite-svelte';
+    import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
+
+	import { Dropdown, DropdownItem, DropdownDivider, DropdownHeader } from 'flowbite-svelte';
+	import { AngleDownOutline} from 'flowbite-svelte-icons';
+	import { userProfileStore } from '$lib/userProfileStore';
+
 	// You can add your script here if you need to handle any logic
+
+    const modalStore = getModalStore();
+
+    let markdownText = writable('');
+    let events = writable([]);
+
+    const addEvent: ModalComponent = { ref: AddEvent };
+
+    const modal: ModalSettings = {
+        type: 'component',
+        component: addEvent
+    };
+
+    let db = getFirestore(app);
+
+    onMount(() => {
+		const eventsCollection = collection(db, 'events');
+		const eventsQuery = query(eventsCollection);
+		const unsubscribe = onSnapshot(
+			eventsQuery,
+			(querySnapshot) => {
+				const eventsData = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+				events.set(eventsData);
+				console.log(eventsData);
+			},
+			(error) => {
+				console.error('Error getting events:', error);
+			}
+		);
+            
+	});
+
+    async function openModal() {
+        modalStore.trigger(modal);
+    }
+
+    $: console.log("EVENTS: ");
+
+    modalStore.close();
+
 </script>
 
 <div class="container mx-auto p-4 grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -9,11 +66,18 @@
         <div class="card bg-white shadow-md rounded-lg p-4 mb-4">
 			<!-- Add margin-bottom here -->
             <h3 class="card-title text-lg font-semibold">Kalender</h3>
-            <ul class="list-disc pl-5">
-                <li>4/20: Get it? Huh</li>
-                <li>6/9: It's a thing</li>
-                <li>7/2: More events</li>
-            </ul>
+            {#if $userProfileStore?.roles.isAdmin}
+                <button type="button" class="btn variant-filled" on:click={openModal}>Tilføj event</button>
+            {/if}
+            <Accordion>
+                {#each $events as event}
+                    <AccordionItem closed>
+                        <svelte:fragment slot="lead">{event.eventStart}</svelte:fragment>
+                        <svelte:fragment slot="summary">{event.title}</svelte:fragment>
+                        <svelte:fragment slot="content">{event.description}</svelte:fragment>
+                    </AccordionItem>
+                {/each}
+            </Accordion>
         </div>
 
         <!-- News/Articles Card -->
