acfcc88c05a0e8e0e10930308293b34708bc77ba
DJMogens <dani788c@gmail.com>
2024-04-29 10:15:53 +0200

    Initial nodeEmailer setup

--- fdca-intranet/src/lib/KontaktDrawer.svelte
+++ fdca-intranet/src/lib/KontaktDrawer.svelte
@@ -1,24 +1,53 @@
-<script>
-                import { Textarea, Toolbar, ToolbarGroup, ToolbarButton, Button } from 'flowbite-svelte';
-                import { PaperClipOutline, MapPinAltSolid, ImageOutline } from 'flowbite-svelte-icons';
+<script lang="ts">
+	import { Button, Textarea } from 'flowbite-svelte';
+    import { getToken } from './login';
+    import { writable } from 'svelte/store';
+
+    let text = writable('');
+    let body = writable('');
+    let subject = writable('');
+
+    async function sendEmail() {
+        try {
+            const token = await getToken();
+            const submissionFormData = new FormData();
+            submissionFormData.append('text', $text);
+            submissionFormData.append('body', $body);
+            submissionFormData.append('subject', $subject);
+
+            // Append the postID to the URL as a parameter
+            const response = await fetch(`/api/sendEmail`, {
+                method: 'POST',
+                headers: {
+                    'X-firebase-token': token
+                },
+                body: submissionFormData
+            });
+        } catch (error) {
+            console.error('Error:', error.message);
+        }
+    }
 </script>
 
-<div
-    id="kontakt-bestyrelse-drawer"
-    class="drawer bg-gray-800 text-black ring-2 ring-gray-700 ring-opacity-100 w-128 h-128 p-4 rounded-xl flex flex-col relative h-full"
->
+<div id="kontakt-bestyrelse-drawer" class="drawer bg-gray-800 text-black ring-2 ring-gray-700 ring-opacity-100 w-128 h-128 p-4 rounded-xl flex flex-col relative h-full">
     <div class="flex flex-col justify-between h-full">
         <div class="text-center p-2">
             <form>
-                <Textarea class="mb-2 text-white" placeholder="Stil bestyrelsen et spørgsmål." style="height: 150px; background-color: #27313e; box-shadow: 0 0 0 2px gray inset;">
-                    <div slot="footer" class="flex items-center justify-between">
-                        <Toolbar embedded>
-                            <ToolbarButton class="text-white"><PaperClipOutline class="w-10 h-10" /></ToolbarButton>
-                            <ToolbarButton class="text-white"><ImageOutline class="w-10 h-10" /></ToolbarButton>
-                        </Toolbar>
-                        <Button class="bg-white text-black py-2 px-8 rounded-full mr-2" type="submit">Send mail</Button>
-                    </div>
-                </Textarea>
+                <Textarea
+                    class="mb-2 text-white"
+                    placeholder="Stil bestyrelsen et spørgsmål."
+                    style="height: 150px; background-color: #27313e; box-shadow: 0 0 0 2px gray inset;"
+                    bind:value={$text}
+                />
+                <div class="flex items-center justify-between">
+                    <Button
+                        class="bg-white text-black py-2 px-8 rounded-full mr-2"
+                        type="submit"
+                        on:click={sendEmail}
+                    >
+                        Send mail
+                    </Button>
+                </div>
             </form>
             <p class="ms-auto text-xs text-gray-500 dark:text-gray-400">
                 Ved at trykke på "Send mail" accepterer du at din mail bliver sendt til bestyrelsen.
---
+++ fdca-intranet/src/routes/api/sendEmail/+server.ts
@@ -0,0 +1,82 @@
+import { json } from '@sveltejs/kit';
+import { admin } from '$lib/firebaseAdmin.server.js';
+import validator from 'validator';
+import { type DecodedIdToken } from 'firebase-admin/auth';
+import nodemailer from 'nodemailer';
+import type { User } from '$lib/user.js';
+import path from 'path';
+import keys from '/secrets/fdca-intranet-dev-test-0fcb3c7d3892.json';
+
+// Initialize nodemailer transporter
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        type: 'OAuth2',
+        user: 'dm@fdca.dk',
+        clientId: keys.client_id,
+        privateKey: keys.private_key,
+        accessUrl: keys.token_uri,
+        serviceClient: keys.client_email
+    }
+});
+
+export async function POST(event) {
+    const firebaseToken = event.request.headers.get('X-firebase-token');
+    if (!firebaseToken) {
+        return json({ error: 'Firebase token not provided' }, { status: 401 });
+    }
+
+    let token: DecodedIdToken;
+    try {
+        token = await admin.auth().verifyIdToken(firebaseToken);
+        console.log('Successfully authenticated Firebase token from user:', token.email);
+    } catch (error) {
+        console.error('Error verifying Firebase token:', error);
+        return json({ error: 'Failed to authenticate Firebase token' }, { status: 403 });
+    }
+
+    const formData = await event.request.formData();
+    const to = formData.get('to')?.toString() || "default@example.com";
+    const subject = formData.get('subject')?.toString() || "No Subject";
+    const body = formData.get('body')?.toString() || "No Content";
+
+    // Validate recipient email format
+    if (!validator.isEmail(to)) {
+        return json({ error: 'Invalid email address provided.' }, { status: 400 });
+    }
+
+    const mailOptions = {
+        from: `dm@fdca.dk`,
+        to: "dm@fdca.dk",
+        subject: "test",
+        text: "test",
+    };
+
+
+
+    try {
+        console.log('Verifying transporter...');
+        await transporter.verify((error, success) => {
+            if (error) {
+                console.error('Verification error:', error);
+            } else {
+                console.log('Verification result:', success);
+            }
+        });
+        console.log('Sending mail...');
+        const info = await transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error('SendMail error:', error);
+            } else {
+                console.log('SendMail result:', info);
+            }
+        });
+        console.log('Message sent:', info.messageId);
+        return json({ success: true, message: 'Email sent successfully', messageId: info.messageId });
+    } catch (error) {
+        console.error('Error sending email:', error);
+        return json({ error: 'Failed to send email' }, { status: 500 });
+    }
+}
