690ec7b4b7efe6a1af7f140a1662d735243cc773
Soya <Axewu_Reitzel@hotmail.com>
2024-04-30 14:48:27 +0200

    Complete refactor of Profile drawer styling and layout

--- fdca-intranet/src/lib/Profile.svelte
+++ fdca-intranet/src/lib/Profile.svelte
@@ -54,7 +54,7 @@
 
 <div
 	id="example-3"
-	class="drawer bg-gray-800 text-white ring-2 ring-gray-700 ring-opacity-100 w-64 p-4 rounded-xl flex flex-col relative h-full"
+	class="drawer bg-gray-800 text-white ring-2 ring-gray-700 ring-opacity-100 w-64 p-5 rounded-xl flex flex-col relative h-full"
 >
 	<div class="flex flex-col justify-between h-full pb-16">
 		<div class="text-center p-2">
@@ -63,46 +63,46 @@
 				alt=""
 				class="h-16 w-16 rounded-full mx-auto"
 			/>
-			<div class="font-semibold my-4">{userDoc?.details?.fullName}</div>
+			<div class="font-semibold my-2">{userDoc?.details?.fullName}</div>
 		</div>
 		<!-- Menu Items -->
 		<ul class="text-sm">
 			<!-- Divider Line -->
-			<hr class="my-2 border-t border-gray-600 h-px bg-gray-600 w-5/7 mx-auto" />
+			<hr class="my-0 border-t border-gray-600 h-px bg-gray-600 w-5/7 mx-auto" />
 			<!-- Phone -->
-			<li class="p-2 rounded-md font-semibold">arbejde</li>
-			<li class="hover:bg-gray-700 p-2 rounded-md font-semibold">{userDoc?.details.arbejde}</li>
+			<li class="p-1 rounded-md font-bold" style="list-style-type: none; font-size: 15px">Arbejde:</li>
+			<li class="hover:bg-gray-700 p-1 rounded-md" style="list-style-type: circle;">{userDoc?.details.arbejde}</li>
 			<!-- Divider Line -->
 			<hr class="my-2 border-t border-gray-600 h-px bg-gray-600 w-5/7 mx-auto" />
 			<!-- Phone -->
-			<li class="p-2 rounded-md font-semibold">Certificates</li>
-			<li class="hover:bg-gray-700 p-2 rounded-md font-semibold">{userDoc?.details.certificate}</li>
+			<li class="p-1 rounded-md font-bold" style="list-style-type: none; font-size: 15px">Certificates:</li>
+			<li class="hover:bg-gray-700 p-1 rounded-md" style="list-style-type: circle;">{userDoc?.details.certificate}</li>
 
 			<!-- Divider Line -->
 			<hr class="my-2 border-t border-gray-600 h-px bg-gray-600 w-5/7 mx-auto" />
 			<!-- Phone -->
-			<li class="p-2 rounded-md font-semibold">Phone</li>
+			<li class="p-1 rounded-md font-bold" style="list-style-type: none; font-size: 15px">Telefon numre:</li>
 			{#each Object.entries(userDoc?.details?.phone || {}) as [key, value]}
 				{#if value && key !== 'work'}
-					<li class="hover:bg-gray-700 p-2 rounded-md font-semibold">{key}: {value}</li>
+					<li class="hover:bg-gray-700 p-1 rounded-md" style="list-style-type: none;">{key}:<br> {value}</li>
 				{/if}
 			{/each}
 
 			<!-- Divider Line -->
 			<hr class="my-2 border-t border-gray-600 h-px bg-gray-600 w-5/7 mx-auto" />
 			<!-- Mail -->
-			<li class="p-2 rounded-md font-semibold">Emails</li>
+			<li class="p-1 rounded-md font-bold" style="list-style-type: none; font-size: 15px">Email-adresser:</li>
 			{#each Object.entries(userDoc?.details?.email || {}) as [key, value]}
 				{#if value && key !== 'work'}
-					<li class="hover:bg-gray-700 p-2 rounded-md font-semibold">{key}: {value}</li>
+					<li class="hover:bg-gray-700 p-1 rounded-md" style="list-style-type: none;"> {key}:<br> <i>{value}</i></li>
 				{/if}
 			{/each}
 
 			<!-- Divider Line -->
 			<hr class="my-2 border-t border-gray-600 h-px bg-gray-600 w-5/7 mx-auto" />
 			<!-- Menu Items -->
-			<li class="p-2 rounded-md font-semibold">Discord User</li>
-			<li class="hover:bg-gray-700 p-2 rounded-md font-semibold">{userDoc?.details.discordName}</li>
+			<li class="p-1 rounded-md font-bold" style="list-style-type: none; font-size: 15px">Discord Username:</li>
+			<li class="hover:bg-gray-700 p-1 rounded-md" style="list-style-type: none;"> {userDoc?.details.discordName}</li>
 			<!-- Divider Line -->
 			<hr class="my-2 border-t border-gray-600 h-px bg-gray-600 w-5/7 mx-auto" />
 		</ul>
@@ -111,13 +111,13 @@
 	<!-- Buttons -->
 	<div class="absolute bottom-2.5 left-1 right-1 p-2 flex justify-between">
 		<button
-			class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+			class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-28"
 			on:click={handleEdit}
 		>
 			Edit
 		</button>
 		<button
-			class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+			class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded w-28"
 			on:click={handleLogout}
 		>
 			Logout
